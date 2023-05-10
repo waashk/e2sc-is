5c619316831b2680896caf126fa030b8a44b0ba9
Washington Luiz <waasluiz@gmail.com>
2023-05-10 13:46:34 -0300

    [+settings] Updating setup

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,6 +1,23 @@
 
-#echo "Installing PythonVEnv"
-#sudo apt install -y python3-venv 
+#echo "Installing Python and env"
+
+sudo apt update --fix-missing -y 
+
+sudo apt install -y --no-install-recommends apt-utils module-init-tools software-properties-common build-essential
+
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+sudo apt update 
+
+sudo apt install -y python3.6 python3.6-dev python3-pip python3-venv 
+
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 
+
+echo $TZ > /etc/timezone
+
+sudo apt install -y --no-install-recommends module-init-tools wget nano curl git ninja-build ccache
+
+
 
 echo "Setting ISENV"
 python3.6 -m venv env

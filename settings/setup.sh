cbf4520ff8f33acc254ec11e8ab81f8a651d946d
Washington Luiz <waasluiz@gmail.com>
2023-05-11 16:35:03 -0300

    [+setting] small fixing on settings

--- settings/setup.sh
+++ settings/setup.sh
@@ -1,5 +1,5 @@
 
-#echo "Installing Python and env"
+echo "Installing Python and env"
 
 sudo apt update --fix-missing -y 
 
@@ -11,11 +11,10 @@ sudo apt update
 
 sudo apt install -y python3.6 python3.6-dev python3-pip python3-venv 
 
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 
-
-echo $TZ > /etc/timezone
-
-sudo apt install -y --no-install-recommends module-init-tools wget nano curl git ninja-build ccache
+#ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 
+#echo $TZ > /etc/timezone
+#
+#sudo apt install -y --no-install-recommends module-init-tools wget nano curl git ninja-build ccache
 
 
 
